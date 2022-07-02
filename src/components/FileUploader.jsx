b67cb772e5f2c5a7006bd63e597347fd5de42bd8
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2022-07-01 20:46:26 -0500

    clear project

--- src/components/FileUploader.jsx
+++ src/components/FileUploader.jsx
@@ -1,37 +1,7 @@
 import { Upload } from 'tabler-icons-react'
-import folderIcon from '@assets/icons/folder.svg'
 import styles from '@styles/FileUploader.module.scss'
 
 const FileUploader = ({ onLoadFiles, validFiles = '.txt' }) => {
-  // const handleCryptFiles = () => {
-  //   files.map(({ file, name }) => {
-  //     const reader = new FileReader()
-  //     reader.readAsText(file)
-  //     reader.onload = async (e) => {
-  //       const content = e.target.result
-  //       const encryptedContent = await encryptFiles(content, 'secret')
-  //       const blob = new Blob([encryptedContent], { type: file.type })
-  //       const url = URL.createObjectURL(blob)
-  //       setFileBlobs((prevState) => {
-  //         return prevState.concat([{ url, name }])
-  //       })
-  //     }
-  //   })
-  // }
-
-  // const checkContent = () => {
-  //   console.log(fileBlobs)
-  // }
-
-  // const downloadFiles = () => {
-  //   fileBlobs.map(({ url, name }) => {
-  //     const a = document.createElement('a')
-  //     a.href = url
-  //     a.download = name
-  //     a.click()
-  //   })
-  // }
-
   return (
     <label className={styles.container} htmlFor="files">
       <Upload size={42} />
